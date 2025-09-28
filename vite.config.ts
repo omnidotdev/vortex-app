8aec533c52d97f20591feeeb7db8bcf9dcf9da8b
Brian Cooper <brian@brian-cooper.com>
2025-09-27 23:16:33 -0700

    feature(vite): add prod URL to allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   server: {
     port: 3000,
+    allowedHosts: ["vortex.omni.dev"],
   },
   plugins: [
     // tailwindcss(),
